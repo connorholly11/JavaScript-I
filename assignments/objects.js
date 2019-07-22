e7d019689082ea2de0b0dff1ccb59d746cb818b2
Connor Holly <connorholly@Connors-MacBook-Pro-2.local>
2019-07-22 17:18:24 -0400

    finished + stretch goal on functions

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,26 +19,99 @@ const example = {
 
 // Write your intern objects here:
 
+//intern 1
+const Mitzi = {
+  "id": 1,
+  "name": "Mitzi",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+}
+
+console.log(Mitzi);
+
+//intern 2
+const Kennan = {
+  "id": 2,
+  "name": "Kennan",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M",
+  
+  speak: function(){
+    return "Hello, my name is Kennan"
+}
+};
+
+console.log(Kennan);
+
+//intern 3
+const Keven = {
+  "id": 3,
+  "name": "Keven",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+
+console.log(Keven);
+
+//intern 4
+const Gannie = {
+  "id": 4,
+  "name": "Gannie",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "M"
+}
+
+console.log(Gannie);
+
+//intern 5
+const Ant = {
+  "id": 5,
+  "name": "Antonietta",
+  "email": "adaine5@samsung.com",
+  "gender": "F",
+
+  multiplyNums: function(num1, num2){
+    return num1 * num2;
+  }
+};
+
+console.log(Ant);
+
+
+
+
+
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(Mitzi.name);
 
 // Kennan's ID
+console.log(Kennan.id);
 
 // Keven's email
+console.log(Keven.email);
 
 // Gannie's name
+console.log(Gannie.name);
 
 // Antonietta's Gender
+console.log(Ant.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+console.log(Kennan.speak());
+
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+
+console.log(Ant.multiplyNums(3,4));
+
+
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -49,7 +122,20 @@ const example = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+// const parent = {
+//   "name": Susan,
+//   "age": 70,
+
+//     const child = {
+//       "name": George,
+//       "age": 50,
+//     }//closes child
+
+//       const grandchild = {
+//         "name": Sam
+//         "age": 30,
+//       }//closes grandchild
+// }
 
 // Log the parent object's name
 
